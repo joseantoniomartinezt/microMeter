f0e7ba9e53194c8c09a89afbabbb1867dca56a42
joseantonio <joseantoniomartinez1904@gmail.com>
2021-11-30 14:56:12 +0100

    first commit

---
+++ src/main/java/com/everis/micrometer/MicroMicroMeterApplication.java
@@ -0,0 +1,13 @@
+package com.everis.micrometer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroMicroMeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroMicroMeterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everis/micrometer/MicroMicroMeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.micrometer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroMicroMeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
