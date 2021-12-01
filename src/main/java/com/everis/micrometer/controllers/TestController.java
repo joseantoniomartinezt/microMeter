f8dfb221597637319a12c84c90f3dda4f606a4f7
joseantonio <joseantoniomartinez1904@gmail.com>
2021-12-01 08:20:03 +0100

    second commit

---
+++ src/main/java/com/everis/micrometer/controllers/TestController.java
@@ -0,0 +1,25 @@
+package com.everis.micrometer.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+
+@RestController
+public class TestController {
+
+	private Counter counter;
+	
+	public TestController(MeterRegistry registry) {
+		this.counter = Counter.builder("invocaciones.test").description("Invocaciones totales").register(registry);
+		
+	}
+	
+	@GetMapping(path = "/helloWorld")
+	public String hellorWorld() {
+		counter.increment();
+		return "Hello World";
+	}
+	
+}
